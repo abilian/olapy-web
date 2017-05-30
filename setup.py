e7e99e3701f0650604384df7af8ef9422f14d86d
mouadh <mouadh.kaabachi@esprit.tn>
2017-05-30 13:25:05 +0200

    update-deps

--- setup.py
+++ setup.py
@@ -9,6 +9,28 @@ from pip.req import parse_requirements
 from setuptools import find_packages, setup
 
 
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+
+
+class PostDevelopCommand(develop):
+    """Post-installation for development mode."""
+    def run(self):
+        # PUT YOUR PRE-INSTALL SCRIPT HERE or CALL A FUNCTION
+        print('111111111111111111111111')
+        develop.run(self)
+        # PUT YOUR POST-INSTALL SCRIPT HERE or CALL A FUNCTION
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        # PUT YOUR PRE-INSTALL SCRIPT HERE or CALL A FUNCTION
+        print('2222222222222222222222222')
+        install.run(self)
+        # PUT YOUR POST-INSTALL SCRIPT HERE or CALL A FUNCTION
+
+
+
 session = PipSession()
 _install_requires = parse_requirements('requirements.txt', session=session)
 install_requires = [str(ir.req) for ir in _install_requires]
@@ -25,6 +47,10 @@ setup(
     long_description=open('README.rst').read(),
     install_requires=install_requires,
     include_package_data=False,
+    cmdclass={
+        'develop': PostDevelopCommand,
+        'install': PostInstallCommand,
+    },
     classifiers=[
         "Programming Language :: Python",
         'Development Status :: 3 - Alpha',
@@ -40,22 +66,22 @@ install_requires = [str(ir.req) for ir in _install_requires]
 
 
 # TODO temp
-os.system('pip install -e git+https://github.com/abilian/olapy.git@b0e89794d508b20c8d2abe60311f5f735be3aa8c#egg=olapy')
+# os.system('pip install -e git+https://github.com/abilian/olapy.git@b0e89794d508b20c8d2abe60311f5f735be3aa8c#egg=olapy')
 
 basedir = expanduser('~')
 if not os.path.isfile(os.path.join(basedir,'olapy-data','olapy.db')):
     # try:
-        # from manage import initdb
-        # initdb()
-    from web.models import User
-
-    from web import db
-    db.create_all()
-    db.session.add(
-        User(username="admin", email="admin@admin.com", password='admin'))
-    db.session.add(
-        User(username="demo", email="demo@demo.com", password="demo"))
-    db.session.commit()
-    print('Initialized the database')
+    from manage import initdb
+    initdb()
+    # from web.models import User
+    #
+    # from web import db
+    # db.create_all()
+    # db.session.add(
+    #     User(username="admin", email="admin@admin.com", password='admin'))
+    # db.session.add(
+    #     User(username="demo", email="demo@demo.com", password="demo"))
+    # db.session.commit()
+    # print('Initialized the database')
     # except:
     #     raise ('unable to create users !')
