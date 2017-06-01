b31d5915c5cf4052c6bd15b00112d65c6a7ab237
mouadh <mouadh.kaabachi@esprit.tn>
2017-06-01 11:53:23 +0200

    fix create db

--- setup.py
+++ setup.py
@@ -6,34 +6,18 @@ from os.path import expanduser
 
 from pip.download import PipSession
 from pip.req import parse_requirements
-from setuptools import find_packages, setup
 
 
-# from setuptools.command.develop import develop
+from setuptools import setup, find_packages
+from setuptools.command.develop import develop
 from setuptools.command.install import install
 
 
-# class PostDevelopCommand(develop):
-#     """Post-installation for development mode."""
-#     def run(self):
-#         # PUT YOUR PRE-INSTALL SCRIPT HERE or CALL A FUNCTION
-#         print('111111111111111111111111')
-#         develop.run(self)
-#
-#         basedir = expanduser('~')
-#         if not os.path.isfile(os.path.join(basedir, 'olapy-data', 'olapy.db')):
-#             # try:
-#             from manage import initdb
-#             initdb()
-#         # PUT YOUR POST-INSTALL SCRIPT HERE or CALL A FUNCTION
-
-class PostInstallCommand(install):
-    """Post-installation for installation mode."""
+class PostDevelopCommand(develop):
+    """Post-installation for development mode."""
     def run(self):
         # PUT YOUR PRE-INSTALL SCRIPT HERE or CALL A FUNCTION
-
-        install.run(self)
-        print('2222222222222222222222222')
+        develop.run(self)
         basedir = expanduser('~')
         if not os.path.isfile(os.path.join(basedir, 'olapy-data', 'olapy.db')):
             # try:
@@ -41,6 +25,17 @@ class PostInstallCommand(install):
             initdb()
         # PUT YOUR POST-INSTALL SCRIPT HERE or CALL A FUNCTION
 
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        # PUT YOUR PRE-INSTALL SCRIPT HERE or CALL A FUNCTION
+        install.run(self)
+        basedir = expanduser('~')
+        if not os.path.isfile(os.path.join(basedir, 'olapy-data', 'olapy.db')):
+            # try:
+            from manage import initdb
+            initdb()
+        # PUT YOUR POST-INSTALL SCRIPT HERE or CALL A FUNCTION
 
 
 session = PipSession()
@@ -55,14 +50,14 @@ setup(
     author_email="contact@abilian.com",
     description="OLAP Engine",
     url='https://github.com/abilian/olapy',
+    cmdclass={
+        'develop': PostDevelopCommand,
+        'install': PostInstallCommand,
+    },
     # TODO fix tox problem with path
     long_description=open('README.rst').read(),
     install_requires=install_requires,
     include_package_data=False,
-    cmdclass={
-        # 'develop': PostDevelopCommand,
-        'install': PostInstallCommand,
-    },
     classifiers=[
         "Programming Language :: Python",
         'Development Status :: 3 - Alpha',
@@ -71,29 +66,3 @@ setup(
         "Programming Language :: Python :: 2.7",
         # "Topic :: Business intelligence",
     ],)
-
-session = PipSession()
-_install_requires = parse_requirements('requirements.txt', session=session)
-install_requires = [str(ir.req) for ir in _install_requires]
-
-
-# TODO temp
-# os.system('pip install -e git+https://github.com/abilian/olapy.git@b0e89794d508b20c8d2abe60311f5f735be3aa8c#egg=olapy')
-#
-# basedir = expanduser('~')
-# if not os.path.isfile(os.path.join(basedir,'olapy-data','olapy.db')):
-#     # try:
-#     from manage import initdb
-#     initdb()
-    # from web.models import User
-    #
-    # from web import db
-    # db.create_all()
-    # db.session.add(
-    #     User(username="admin", email="admin@admin.com", password='admin'))
-    # db.session.add(
-    #     User(username="demo", email="demo@demo.com", password="demo"))
-    # db.session.commit()
-    # print('Initialized the database')
-    # except:
-    #     raise ('unable to create users !')
