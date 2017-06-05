8d19246fa696942eefb0014c5355c425f3105aab
mouadh <mouadh.kaabachi@esprit.tn>
2017-06-05 14:03:59 +0200

    update setup

--- setup.py
+++ setup.py
@@ -8,34 +8,6 @@ from pip.req import parse_requirements
 
 
 from setuptools import setup, find_packages
-# from setuptools.command.develop import develop
-# from setuptools.command.install import install
-
-
-# class PostDevelopCommand(develop):
-#     """Post-installation for development mode."""
-#     def run(self):
-#         # PUT YOUR PRE-INSTALL SCRIPT HERE or CALL A FUNCTION
-#         # os.environ['olapy_path'] = "/home/mouadh/Musique2"
-#         develop.run(self)
-#         if not os.path.isfile(os.path.join(os.environ['OLAPY_PATH'], 'olapy-data', 'olapy.db')):
-#         # if not os.path.isfile(os.path.join(basedir, 'olapy-data', 'olapy.db')):
-#             # try:
-#             from .manage import initdb
-#             initdb()
-#         # PUT YOUR POST-INSTALL SCRIPT HERE or CALL A FUNCTION
-#
-# class PostInstallCommand(install):
-#     """Post-installation for installation mode."""
-#     def run(self):
-#         # PUT YOUR PRE-INSTALL SCRIPT HERE or CALL A FUNCTION
-#         install.run(self)
-#         # basedir = expanduser('~')
-#         # if not os.path.isfile(os.path.join(basedir, 'olapy-data', 'olapy.db')):
-#         #     # try:
-#         #     from manage import initdb
-#         #     initdb()
-        # PUT YOUR POST-INSTALL SCRIPT HERE or CALL A FUNCTION
 
 session = PipSession()
 _install_requires = parse_requirements('requirements.txt', session=session)
@@ -52,10 +24,6 @@ setup(
     # TODO fix tox problem with path
     long_description=open('README.rst').read(),
     install_requires=install_requires,
-    # cmdclass={
-    #     'develop': PostDevelopCommand,
-    #     'install': PostInstallCommand,
-    # },
     include_package_data=False,
     classifiers=[
         "Programming Language :: Python",
@@ -68,13 +36,14 @@ setup(
 
 
 from web import app
-# os.environ['OLAPY_PATH'] = app.instance_path
-os.system("export OLAPY_PATH=" + app.instance_path + " ")
+# TODO if server deployment do this
+os.system('export OLAPY_PATH=' + app.instance_path + ' ')
 os.environ['OLAPY_PATH'] = app.instance_path
 os.system('pip install -e file:///home/mouadh/PycharmProjects/olapy/olapy')
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(os.environ['OLAPY_PATH'],'olapy-data',
 #                                                                     'olapy.db')
-if not os.path.isfile(os.path.join(os.environ['OLAPY_PATH'], 'olapy-data', 'olapy.db')):
+# if not os.path.isfile(os.path.join(os.environ['OLAPY_PATH'], 'olapy-data', 'olapy.db')):
+if not os.path.isfile(os.path.join(app.instance_path, 'olapy-data', 'olapy.db')):
     from manage import initdb
     initdb()
