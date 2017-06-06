98250fbd158a004af3a4c913bb21806d474ccce5
mouadh <mouadh.kaabachi@esprit.tn>
2017-06-06 12:05:34 +0200

    export olapy_path (child process)

--- setup.py
+++ setup.py
@@ -8,6 +8,60 @@ from pip.req import parse_requirements
 
 
 from setuptools import setup, find_packages
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+
+
+class PostDevelopCommand(develop):
+    """Post-installation for development mode."""
+    def run(self):
+        # PUT YOUR PRE-INSTALL SCRIPT HERE or CALL A FUNCTION
+        develop.run(self)
+        from web import app
+        # TODO if server deployment do this
+        os.system('export OLAPY_PATH=' + app.instance_path + ' ')
+        os.environ['OLAPY_PATH'] = app.instance_path
+
+        with open('/home/mouadh/file3.txt',mode='w+') as f:
+            f.write(str(app.instance_path))
+
+        # os.system('pip install -e file:///home/mouadh/PycharmProjects/olapy/olapy')
+        os.system(
+            'pip install -e git+https://github.com/abilian/olapy.git@93df06cda679491e11178dedfee6aa657965890e#egg=olapy')
+        # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(os.environ['OLAPY_PATH'],'olapy-data',
+        #                                                                     'olapy.db')
+        # if not os.path.isfile(os.path.join(os.environ['OLAPY_PATH'], 'olapy-data', 'olapy.db')):
+        if not os.path.isfile(os.path.join(app.instance_path, 'olapy-data', 'olapy.db')):
+            from manage import initdb
+            initdb()
+
+            # PUT YOUR POST-INSTALL SCRIPT HERE or CALL A FUNCTION
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        # PUT YOUR PRE-INSTALL SCRIPT HERE or CALL A FUNCTION
+        install.run(self)
+        from web import app
+        # TODO if server deployment do this
+        os.system('export OLAPY_PATH=' + app.instance_path + ' ')
+        os.environ['OLAPY_PATH'] = app.instance_path
+
+        with open('/home/mouadh/file1.txt',mode='w+') as f:
+            f.write(str(app.instance_path))
+
+        # os.system('pip install -e file:///home/mouadh/PycharmProjects/olapy/olapy')
+        os.system(
+            'pip install -e git+https://github.com/abilian/olapy.git@93df06cda679491e11178dedfee6aa657965890e#egg=olapy')
+        # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(os.environ['OLAPY_PATH'],'olapy-data',
+        #                                                                     'olapy.db')
+        # if not os.path.isfile(os.path.join(os.environ['OLAPY_PATH'], 'olapy-data', 'olapy.db')):
+        if not os.path.isfile(os.path.join(app.instance_path, 'olapy-data', 'olapy.db')):
+            from manage import initdb
+            initdb()
+            # PUT YOUR POST-INSTALL SCRIPT HERE or CALL A FUNCTION
+
+
 
 session = PipSession()
 _install_requires = parse_requirements('requirements.txt', session=session)
@@ -25,6 +79,10 @@ setup(
     long_description=open('README.rst').read(),
     install_requires=install_requires,
     include_package_data=False,
+    cmdclass={
+        'develop': PostDevelopCommand,
+        'install': PostInstallCommand,
+    },
     classifiers=[
         "Programming Language :: Python",
         'Development Status :: 3 - Alpha',
@@ -33,18 +91,20 @@ setup(
         "Programming Language :: Python :: 2.7",
         # "Topic :: Business intelligence",
     ],)
-
-
-from web import app
-# TODO if server deployment do this
+#
+# from web import app
+# # TODO if server deployment do this
 # os.system('export OLAPY_PATH=' + app.instance_path + ' ')
-os.environ['OLAPY_PATH'] = app.instance_path
-
+# os.environ['OLAPY_PATH'] = app.instance_path
+#
+# with open('/home/mouadh/file2.txt', mode='w+') as f:
+#     f.write(str(app.instance_path))
+#
 # os.system('pip install -e file:///home/mouadh/PycharmProjects/olapy/olapy')
-os.system('pip install -e git+https://github.com/abilian/olapy-web.git@93df06cda679491e11178dedfee6aa657965890e#egg=olapy_web')
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(os.environ['OLAPY_PATH'],'olapy-data',
-#                                                                     'olapy.db')
-# if not os.path.isfile(os.path.join(os.environ['OLAPY_PATH'], 'olapy-data', 'olapy.db')):
-if not os.path.isfile(os.path.join(app.instance_path, 'olapy-data', 'olapy.db')):
-    from manage import initdb
-    initdb()
+# # os.system('pip install -e git+https://github.com/abilian/olapy.git@93df06cda679491e11178dedfee6aa657965890e#egg=olapy')
+# # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(os.environ['OLAPY_PATH'],'olapy-data',
+# #                                                                     'olapy.db')
+# # if not os.path.isfile(os.path.join(os.environ['OLAPY_PATH'], 'olapy-data', 'olapy.db')):
+# if not os.path.isfile(os.path.join(app.instance_path, 'olapy-data', 'olapy.db')):
+#     from manage import initdb
+#     initdb()
