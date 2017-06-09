da98f72ba2f43994177dd5218eb5780c078eb874
mouadh <mouadh.kaabachi@esprit.tn>
2017-06-09 13:16:34 +0200

    change to flask cli

--- manage.py
+++ manage.py
@@ -1,13 +1,16 @@
 from __future__ import absolute_import, division, print_function
 
+import click
+from flask.cli import pass_script_info
 from flask_script import Manager, prompt_bool, Server
 
-from web import app, db
-from web.models import User
+from .web import app, db
+from .web.models import User
 
-manager = Manager(app)
+# manager = Manager(app)
 
-@manager.command
+@app.cli.command(short_help='Initialize database')
+# @manager.command
 def initdb():
     db.create_all()
     db.session.add(
@@ -17,14 +20,25 @@ def initdb():
     db.session.commit()
     print('Initialized the database')
 
-
-@manager.command
+@app.cli.command(short_help='Drop database')
+# @manager.command
 def dropdb():
     if prompt_bool('Are you sure you want to lose all your data? '):
         db.drop_all()
         print('Dropped the database')
 
 
+# @app.cli.command(short_help='Run Server')
+@click.command('run', short_help='Runs a development server.')
+@click.option('--host', '-h', default='0.0.0.0',
+                  help='The interface to bind to.')
+@click.option('--port', '-p', default=5000,
+                  help='The port to bind to.')
+@pass_script_info
+def runserver(host, port):
+    # manager.add_command('runserver', Server(host=host, port=port))
+    # manager.run()
+    app.run(host=host, port=port)
+
 if __name__ == '__main__':
-    manager.add_command('runserver', Server(host='0.0.0.0', port=5000))
-    manager.run()
+    runserver()
