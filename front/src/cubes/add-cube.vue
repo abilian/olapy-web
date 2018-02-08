1d06b52a8808622fb54b9fb9d0a84574d0746c04
mouadhkaabachi <mouadh.kaabachi@esprit.tn>
2018-02-08 18:36:27 +0100

    update

--- front/src/cubes/add-cube.vue
+++ front/src/cubes/add-cube.vue
@@ -5,25 +5,33 @@
         <div class="modal-container">
 
           <div class="modal-header">
-            <!--<slot name="header">-->
-            <!--default header-->
-            <!--</slot>-->
             <label>
               Cube Name :
               <input type="text" name="cubeName">
             </label>
+
+            <label style="margin-left: 70px">
+              Source :
+              <select v-model="source">
+                <option disabled value="">Choisissez</option>
+                <option>CSV</option>
+                <option>DataBase</option>
+              </select>
+            </label>
+
           </div>
 
           <div class="modal-body">
             <slot name="body">
-              <uploadCsvFiles></uploadCsvFiles>
+              <uploadCsvFiles v-show="source == 'CSV'"></uploadCsvFiles>
+              <connectDb v-show="source == 'DataBase'"></connectDb>
             </slot>
           </div>
 
           <div class="modal-footer">
             <slot name="footer">
               <button class="modal-default-button" @click="$emit('close')">
-                OK
+                Next
               </button>
             </slot>
           </div>
@@ -37,10 +45,17 @@
 <script>
 
   import uploadFiles from './upload-files';
+  import connectDb from './connect-db';
 
   export default {
-    components : {
-      uploadCsvFiles: uploadFiles
+    data: function () {
+      return {
+        source: ''
+      }
+    },
+    components: {
+      uploadCsvFiles: uploadFiles,
+      connectDb: connectDb
     }
 
   }
---
+++ front/src/cubes/connect-db.vue
@@ -0,0 +1,70 @@
+<template>
+
+  <div>
+    <div style="position: center">
+      <label>
+        engine :
+        <select v-model="engine">
+          <option disabled value="">Choisissez</option>
+          <option>Postgres</option>
+          <option>Mysql</option>
+          <option>Oracle</option>
+          <option>SQL Server</option>
+        </select>
+      </label>
+      <br>
+
+      <label>
+        Server Name :
+        <input type="text" :value="servername" name="servername">
+      </label>
+      <br>
+
+      <label>
+        Port :
+        <input type="text" :value="port" name="servername">
+      </label>
+      <br>
+
+      <label>
+        Database :
+        <input type="text" :value="database" name="servername">
+      </label>
+      <br>
+
+      <label>
+        User Name :
+        <input type="text" :value="username" name="servername">
+      </label>
+      <br>
+
+      <label>
+        Password :
+        <input type="text" :value="password" name="servername">
+      </label>
+      <br>
+    </div>
+
+  </div>
+
+</template>
+
+<script>
+  export default {
+    data: function () {
+      return {
+        engine: '',
+        servername: 'localhost',
+        port: '',
+        database: '',
+        username: '',
+        password: '',
+
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
