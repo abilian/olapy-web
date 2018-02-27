5a7f2b5702bdfc0ef19746ddb0239adbfc4d2857
mouadhkaabachi <mouadh.kaabachi@esprit.tn>
2018-02-27 14:20:24 +0100

    update setup for build js

--- setup.py
+++ setup.py
@@ -1,13 +1,57 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function
 
+from distutils import log
+from distutils.command.sdist import sdist
+
 from pip.download import PipSession
+from subprocess import check_output
 from pip.req import parse_requirements
-from setuptools import find_packages, setup
+from os.path import dirname, join, realpath
+from setuptools import find_packages, setup, Command
+from setuptools.command.develop import develop
 
 session = PipSession()
 _install_requires = parse_requirements('requirements.in', session=session)
 install_requires = [str(ir.req) for ir in _install_requires]
+
+ROOT = realpath(join(dirname(__file__)))
+
+
+class BuildStatic(Command):
+    description = 'build static (front-end) files'
+
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        front_dir = join(ROOT, 'front')
+
+        log.info("running 'npm install --quiet'")
+        check_output(['npm', 'install', '--quiet'], cwd=front_dir)
+
+        log.info("running 'npm run build'")
+        check_output(['npm', 'run', 'build'], cwd=front_dir)
+
+
+class SdistWithBuildStatic(sdist):
+
+    def make_distribution(self):
+        self.run_command('build_static')
+        return sdist.make_distribution(self)
+
+
+class DevelopWithBuildStatic(develop):
+
+    def install_for_development(self):
+        self.run_command('build_static')
+        return develop.install_for_development(self)
+
 
 setup(
     name='olapy_web',
@@ -17,7 +61,6 @@ setup(
     author_email="contact@abilian.com",
     description="Front-end to OLAP Engine",
     url='https://github.com/abilian/olapy-web',
-    # TODO fix tox problem with path
     long_description=open('README.rst').read(),
     install_requires=install_requires,
     include_package_data=False,
@@ -29,4 +72,9 @@ setup(
         "Programming Language :: Python :: 2.7",
         # "Topic :: Business intelligence",
     ],
+    cmdclass={
+        'build_static': BuildStatic,
+        'develop': DevelopWithBuildStatic,
+        'sdist': SdistWithBuildStatic,
+    }
 )
