400f68aee968b9e50086e71e18eef48669560df4
mouadhkaabachi <mouadh.kaabachi@esprit.tn>
2018-04-10 16:35:49 +0100

    refactor : separate cubes api dash api

--- app/api/views.py
+++ app/api/views.py
@@ -478,23 +478,3 @@ def get_cube_columns(cube_name):
     executor = _load_cube(cube_name)
     return jsonify([column for column in executor.star_schema_dataframe.columns if
                     column.lower()[-3:] != '_id' and column not in executor.measures])
-
-
-@api('dashboard/save', methods=['POST'])
-@login_required
-def save_dashboard():
-    request_data = request.get_json()
-    user_dashboard = User.query.filter(User.id == current_user.id).first().dashboards
-    if user_dashboard:
-        # update dashboard
-        user_dashboard.name = request_data['dashboardName']
-        user_dashboard.content = request_data['dashboardContent']
-    else:
-        # add new cube
-        cube = Dashboard(name=request_data['dashboardName'],
-                         content=request_data['dashboardContent'],
-                         user_id=current_user.id
-                         )
-        db.session.add(cube)
-    db.session.commit()
-    return jsonify({'success': True}), 200
