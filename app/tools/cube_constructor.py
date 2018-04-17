5725736c24450d3b729f152d79be0aa26700447b
mouadhkaabachi <mouadh.kaabachi@esprit.tn>
2018-04-17 09:31:00 +0100

    remove old dashboard

--- app/tools/cube_constructor.py
+++ app/tools/cube_constructor.py
@@ -1,29 +1,155 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-from olapy.core.mdx.executor.execute import SUPPORTED_FILES, SUPPORTED_DATABASES, MdxEngine
-from olapy.core.mdx.executor.execute_config_file import construct_web_star_schema_config_file
-from olapy.core.mdx.executor.execute_csv_files import construct_star_schema_csv_files
-from olapy.core.mdx.executor.execute_db import construct_star_schema_db
+import os
 
+import yaml
 
-class CubeConstructor:
+# TEMPORARY JUST FOR QUERY BUILDER
+class ConfigParser:
+    def __init__(self, web_config_file_name=None):
 
-    def construct_star_schema_from_config(self, config_file_parser):
+        if web_config_file_name:
+            self.web_config_file_name = web_config_file_name
+        else:
+            self.web_config_file_name = 'web_cube_config.yml'
+
+    def config_file_exists(self):
         """
-        There is two different configuration, one for excel 'cubes-config.xml', \
-        and the other for the web 'web_cube_config.xml' (if you want to use olapy-web), they are a bit different.
-        :param config_file_parser: star schema Dataframe
-        :return:
+        Check whether the config file exists or not.
+        :param client_type: excel config file or web
+        :return: True | False
         """
-        fusion = None
-        for cubes in config_file_parser.construct_cubes():
-            if cubes.source.upper() in SUPPORTED_FILES + SUPPORTED_DATABASES:
-                # todo clean!!!!!
-                if cubes.facts:
-                    fusion = construct_web_star_schema_config_file(self, cubes)
-                # todo clean!!!!! # todo clean!!!!! # todo clean!!!!!
-                elif cubes.source.upper() in SUPPORTED_FILES and cubes.name in MdxEngine.csv_files_cubes:
-                    fusion = construct_star_schema_csv_files(self)
-                elif cubes.source.upper() in SUPPORTED_DATABASES and cubes.name in MdxEngine.from_db_cubes:
-                    fusion = construct_star_schema_db(self)
-        return fusion
+
+        return os.path.isfile(self.web_config_file_name)
+
+    def get_cubes_names(self):
+        """Get all cubes names in the config file.
+        :return: dict with dict name as key and cube source as value (csv | postgres | mysql | oracle | mssql)
+        """
+        file_path = self.web_config_file_name
+        with open(file_path) as config_file:
+            config = yaml.load(config_file)
+
+            try:
+                return {config['name']: config['source']}
+            except BaseException:  # pragma: no cover
+                raise ValueError('missed name or source tags')
+
+    def construct_web_dashboard(self):
+        """
+        Parse olapy-web config web file.
+        Config file used if you want to show only some measures, dimensions,
+        columns... in excel
+        Config file should be under '~/olapy-data/cubes/web-cubes-config.xml'
+        name : foodmart_with_config            # csv folder name or db name
+        source : csv                           # csv | postgres | mysql ...
+        facts :                                # facts table name
+          table_name : food_facts
+          keys:                                # primary keys
+            columns_names :
+              - product_id
+              - warehouse_id
+              - store_id
+            refs :                             # keys refs (example : product_id ref to column id from table Product...)
+              - Product.id
+              - Warehouse.id
+              - Store.id
+          measures :                  # by default, all number type columns in facts table, or you can specify them here
+            - units_ordered
+            - units_shipped
+            - supply_time
+        tables :                               #  additional columns to keep other than measures and ids columns :
+          - table :
+              name : Product
+              columns : id,brand_name,product_name
+              new_names :
+                product_name : Product_Name
+          - table :
+              name : Store
+              columns : id,store_type,store_name,store_city
+          - table :
+              name : Warehouse
+              columns : id,warehouse_name,warehouse_city,warehouse_country
+        # IMPORTANT !! columns and rows names must be specified as above with their new names
+        # EXAMPLE <new_name old_column_name="label">Pole leader</new_name>, you put Pole leader
+        # marches,axes_de_developpement,statut_pour_book are columns from facts table
+        Dashboard :
+          Global_table:
+            columns :
+              - warehouse_name
+              - warehouse_country
+              - store_type
+              - store_name
+              - brand_name
+            rows :
+              - store_city
+          PieCharts :
+            - store_city
+            - store_type
+            - warehouse_city
+          BarCharts :
+            - warehouse_country
+        #  LineCharts :                                             # Preferably with time/date (or sequenced) tables
+        #    table : Year
+        #    columns :
+        #      - 2010
+        #      - 2011
+        :return: dashboard (dict)
+        """
+        with open(self.web_config_file_name) as config_file:
+            config = yaml.load(config_file)
+
+        return {
+            'global_table': config['Dashboard']['Global_table'],
+            'PieCharts': config['Dashboard']['PieCharts'],
+            'BarCharts': config['Dashboard']['BarCharts'],
+            'LineCharts': {
+                config['Dashboard']['LineCharts']['table']:
+                    config['Dashboard']['LineCharts']['columns'] if
+                    'columns' in config['Dashboard']['LineCharts'] else 'ALL'
+            } if 'LineCharts' in config['Dashboard'] else {}
+        }
+
+    def construct_cubes(self):
+        """
+        Construct parser cube obj (which can ben passed to MdxEngine) for web
+        :return: Cube obj
+        """
+        if not self.config_file_exists():
+            raise ValueError("Config file doesn't exist")
+        with open(self.web_config_file_name) as config_file:
+            config = yaml.load(config_file)
+
+            if 'facts' in config:
+                facts = {
+                    'table_name': config['facts']['table_name'],
+                    'keys': dict(
+                        zip(config['facts']['keys']['columns_names'],
+                            config['facts']['keys']['refs'])),
+                    'measures': config['facts']['measures'],
+                    'columns': config['facts']['columns']
+                    if 'columns' in config['facts'] else '',
+                }
+
+            else:
+                facts = []
+
+            if 'tables' in config:
+                tables = [
+                    {
+                        'name': table['table']['name'],
+                        'columns': table['table']['columns'],
+                        'new_names': {new_col
+                                      for new_col in table['table']['new_names']} if 'new_names' in table[
+                            'table'] else {},
+                    } for table in config['tables']
+                ]
+            else:
+                tables = []
+
+        return {
+            'name': config['name'],
+            'source': config['source'],
+            'facts': facts,
+            'tables': tables
+        }
--- app/views.py
+++ app/views.py
@@ -7,32 +7,18 @@ import os
 from six import text_type
 from typing import Any, Union
 
-import numpy as np
-import pandas as pd
 from flask import Blueprint, Response, current_app, flash, redirect, \
     render_template, request, url_for
 from flask_login import current_user, login_required, login_user, logout_user
 from olapy.core.mdx.executor.execute import MdxEngine
 
-from app.tools.config_file_parser import ConfigParser
+from app.tools.cube_constructor import ConfigParser
 from .extensions import login_manager
 from .forms import LoginForm
 from .models import User
-import importlib
 
 blueprint = Blueprint('main', __name__, template_folder='templates')
 route = blueprint.route
-
-
-def _build_charts(dashboard, executer):
-    graphs = {}
-    for chart_type, chart_attributs in dashboard.items():
-        if chart_type == 'global_table':
-            continue
-        ChartClass = getattr(importlib.import_module("app.tools.models"), chart_type[:-1])
-        chart = ChartClass(executer, chart_attributs)
-        graphs[chart_type.lower().replace('charts', '')] = chart.gen_graphs()
-    return graphs
 
 
 @login_manager.user_loader
@@ -66,7 +52,7 @@ def login():
             # next to hold the the page that the user tries to visite
 
             next_url = request.args.get('next') or url_for(
-                'dashboard', user=current_user)
+                'query_builder', user=current_user)
             return redirect(next_url)
 
         flash('incorrect username or password')
@@ -82,45 +68,6 @@ def logout():
     logout_user()
     return redirect(url_for('.login'))
 
-
-@route('/dashboard', methods=['GET', 'POST'])
-@login_required
-def dashboard():
-    # type: () -> text_type
-    """Generate Dashboard with charts from web_config_file.
-    """
-    olapy_data_location = os.path.join(current_app.instance_path, 'olapy-data')
-
-    web_config_file_path = os.path.join(olapy_data_location, 'cubes', 'web_cube_config.yml')
-    config = ConfigParser(web_config_file_path)
-    dashboard = config.construct_web_dashboard()
-
-    config = ConfigParser(web_config_file_path)
-    cube_config_file = config.construct_cubes()
-
-    executor = MdxEngine(cube_config=cube_config_file,
-                         olapy_data_location=olapy_data_location)
-    executor.load_cube(cube_config_file['name'])
-
-    if not dashboard:
-        return ('<h3> your config file (' + web_config_file_path +
-                ') does not contains dashboard section </h3>')
-
-    graphs = _build_charts(dashboard, executor)
-    # todo margins = True ( prob )
-    pivot_table_df = pd.pivot_table(
-        executor.star_schema_dataframe,
-        values=executor.measures,
-        index=dashboard['global_table']['columns'],
-        columns=dashboard['global_table']['rows'],
-        aggfunc=np.sum)
-    return render_template(
-        'dashboard.html',
-        table_result=pivot_table_df.fillna('').to_html(classes=[
-            'table m-0 table-primary table-colored table-bordered table-hover table-striped display'
-        ]),
-        graphs=graphs,
-        user=current_user)
 
 
 @route('/query_builder', methods=['GET', 'POST'])
