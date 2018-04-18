c9d32f38de41f20dd7b13c15b173357584d23914
mouadhkaabachi <mouadh.kaabachi@esprit.tn>
2018-04-18 12:54:02 +0100

    test add cube

---
+++ tests/unit/test_cubes.py
@@ -0,0 +1,24 @@
+import json
+from os import listdir
+
+from os.path import isfile, join
+
+TEST_CUBE = 'tests/test_cubes/sales'
+
+
+def test_add_cube(client):
+    files = [
+        open(join(TEST_CUBE, file), 'r')
+        for file in listdir(TEST_CUBE) if isfile(join(TEST_CUBE, file))
+    ]
+
+    response = client.post('api/cubes/add', data={
+        'files': files,
+    })
+    result = json.loads(response.data)
+    assert result == {u'facts': None,
+                      u'measures': None,
+                      u'dimensions': [u'tests/test_cubes/sales/Time.csv',
+                                      u'tests/test_cubes/sales/Facts.csv',
+                                      u'tests/test_cubes/sales/Product.csv',
+                                      u'tests/test_cubes/sales/Geography.csv']}
