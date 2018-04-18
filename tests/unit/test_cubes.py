51726daad22079115a08bdb30f0ae5b296695edd
mouadhkaabachi <mouadh.kaabachi@esprit.tn>
2018-04-18 15:39:54 +0100

    rename test

--- tests/unit/test_cubes.py
+++ tests/unit/test_cubes.py
@@ -6,18 +6,21 @@ from os.path import isfile, join
 TEST_CUBE = 'tests/test_cubes/sales'
 
 
-def test_add_cube(client):
+def test_upload_cube(client):
     files = [
-        open(join(TEST_CUBE, file), 'rb')
-        for file in listdir(TEST_CUBE) if isfile(join(TEST_CUBE, file))
+        open(join(TEST_CUBE, file), 'rb') for file in listdir(TEST_CUBE)
+        if isfile(join(TEST_CUBE, file))
     ]
 
-    response = client.post('api/cubes/add', data={
-        'files': files,
-    })
+    response = client.post(
+        'api/cubes/add', data={
+            'files': files,
+        })
     result = json.loads(response.data)
 
-    assert sorted(result['dimensions']) == sorted([u'tests/test_cubes/sales/Time.csv',
-                                                   u'tests/test_cubes/sales/Facts.csv',
-                                                   u'tests/test_cubes/sales/Product.csv',
-                                                   u'tests/test_cubes/sales/Geography.csv'])
+    assert sorted(result['dimensions']) == sorted([
+        u'tests/test_cubes/sales/Time.csv',
+        u'tests/test_cubes/sales/Facts.csv',
+        u'tests/test_cubes/sales/Product.csv',
+        u'tests/test_cubes/sales/Geography.csv'
+    ])
