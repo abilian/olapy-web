b2c4c3e1dbb9485a405ac1e3d9d14913f0872adf
mouadhkaabachi <mouadh.kaabachi@esprit.tn>
2018-04-18 18:33:11 +0100

    lint

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,8 @@ from app.extensions import db as _db
 from app.models import User
 
 TEST_DATABASE_URI = 'sqlite://'  # output db, in which we will save our cube, users....
-SQLA_URI_TEST = "postgresql://postgres:root@localhost/olapy_web_test"  # input db from which we will get our tables, todo sqlite not working fine in web, so this this temp until we fix this
+SQLA_URI_TEST = "postgresql://postgres:root@localhost/olapy_web_test"  # input db from which we will get our tables,\
+#  todo sqlite not working fine in web, so this this temp until we fix this
 
 
 @fixture(scope='module')
