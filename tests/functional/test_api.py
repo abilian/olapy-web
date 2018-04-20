149a4d57b0884d8dd7520fc85519140ec32a8ef8
mouadhkaabachi <mouadh.kaabachi@esprit.tn>
2018-04-20 19:23:13 +0100

    rename variables

--- tests/functional/test_api.py
+++ tests/functional/test_api.py
@@ -6,11 +6,8 @@ from os.path import isfile, join
 
 from tests.utils import chart_data
 
-TEST_CUBE = 'tests/demo_csv_cubes/sales'
-CSV_TEST_CUBE = u'test'
-TEST_CUSTOM_CUBE = 'tests/demo_csv_cubes/foodmart_with_config'
-CSV_CUSTOM_TEST_CUBE = u'custom_test'
-DB_TEST_CUBE = u'olapy_web_test'
+CUBE_PATH = 'tests/demo_csv_cubes/sales'
+CUSTOM_CUBE_PATH = 'tests/demo_csv_cubes/foodmart_with_config'
 
 
 def test_add_csv_cube(client):
@@ -18,24 +15,24 @@ def test_add_csv_cube(client):
         client.post('/login', data=dict(username="admin", password="admin"))
 
         files = [
-            open(join(TEST_CUBE, file), 'rb') for file in listdir(TEST_CUBE)
-            if isfile(join(TEST_CUBE, file))
+            open(join(CUBE_PATH, file), 'rb') for file in listdir(CUBE_PATH)
+            if isfile(join(CUBE_PATH, file))
         ]
 
         client.post('api/cubes/add', data={'files': files})
-        request_data = {'customCube': False, 'cubeName': CSV_TEST_CUBE}
+        request_data = {'customCube': False, 'cubeName': 'test'}
         client.post('api/cubes/confirm_cube', data=json.dumps(request_data))
         client.post('api/cubes/clean_tmp_dir')
         added_cube_result = client.get('api/cubes').data
         result = json.loads(added_cube_result)
-        assert CSV_TEST_CUBE in result
+        assert 'test' in result
 
 
 def test_add_custom_csv_cube(client):
     with client:
         client.post('/login', data=dict(username="admin", password="admin"))
         current_dir = os.getcwd()
-        os.chdir(TEST_CUSTOM_CUBE)  # to send files to server with their real names, not names as path
+        os.chdir(CUSTOM_CUBE_PATH)  # to send files to server with their real names, not names as path
         files = [
             open(file, 'rb') for file in listdir(os.getcwd()) if isfile(file)
         ]
@@ -43,7 +40,7 @@ def test_add_custom_csv_cube(client):
         client.post('api/cubes/add', data={'files': files})
 
         request_data = {
-            'cubeName': CSV_CUSTOM_TEST_CUBE,
+            'cubeName': 'custom_test',
             'factsTable': 'food_facts.csv',
             'tablesAndColumnsResult': {
                 'Product.csv': {
@@ -59,13 +56,13 @@ def test_add_custom_csv_cube(client):
         client.post(
             'api/cubes/construct_custom_cube', data=json.dumps(request_data))
 
-        request_data = {'cubeName': CSV_CUSTOM_TEST_CUBE, 'customCube': True}
+        request_data = {'cubeName': 'custom_test', 'customCube': True}
 
         client.post('api/cubes/confirm_cube', data=json.dumps(request_data))
         client.post('api/cubes/clean_tmp_dir')
         added_cube_result = client.get('api/cubes').data
         result = json.loads(added_cube_result)
-        assert CSV_CUSTOM_TEST_CUBE in result
+        assert 'custom_test' in result
 
 
 def test_add_db_cube(client):
@@ -73,7 +70,7 @@ def test_add_db_cube(client):
         client.post('/login', data=dict(username="admin", password="admin"))
         #  in the web , ypu don't put a string connection, instead each connexion param separately
         db_credentials = dict(
-            selectCube=DB_TEST_CUBE,
+            selectCube='olapy_web_test',
             engine='postgres',
             servername='localhost',
             port='5432',
@@ -90,7 +87,7 @@ def test_add_db_cube(client):
 
         added_cube_result = client.get('api/cubes').data
         result = json.loads(added_cube_result)
-        assert DB_TEST_CUBE in result
+        assert 'olapy_web_test' in result
 
 
 def test_add_dashboard(client):
