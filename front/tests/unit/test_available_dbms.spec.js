b838f9857d8e9b1fe7ed40d677af25812ce0cc51
mouadhkaabachi <mouadh.kaabachi@esprit.tn>
2018-04-23 18:23:58 +0100

    change first test

--- front/tests/unit/test_available_dbms.spec.js
+++ front/tests/unit/test_available_dbms.spec.js
@@ -1,15 +1,15 @@
 import { mount } from "vue-test-utils";
-import ConnectDB from "@/cubes/add_cube/connect-db";
+import ConnectDB from "@/cubes/base-schema-options";
 
 describe("ConnectDB", () => {
   const wrapper = mount(ConnectDB);
 
-  it("fait le rendu correctement", () => {
-    expect(wrapper.html()).toContain(
-      "<option>Postgres</option>" +
-        "<option>Mysql</option>" +
-        "<option>Oracle</option>" +
-        "<option>SQL Server</option>"
-    );
+  const vm = wrapper.vm;
+
+  it("check base-schema-options data", () => {
+    expect(vm.modalToShow).toBe("first");
+    expect(vm.showModal).toBe(false);
+    expect(vm.cube).toBe("");
+    expect(vm.dbConfig).toBe("");
   });
 });
