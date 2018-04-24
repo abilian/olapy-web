02f09a14285fb24e14ae2f65e759ebe0073ec6a8
mouadhkaabachi <mouadh.kaabachi@esprit.tn>
2018-04-24 19:22:37 +0100

    test e2e: select databases

--- front/tests/e2e/test_login.spec.js
+++ front/tests/e2e/test_login.spec.js
@@ -12,4 +12,32 @@ test("Test Login", async t => {
     .typeText(password, "admin")
     .click(loginBtn);
   await t.expect(loginBtn.exists).notOk();
+
+  const addCubeBtn = Selector("#show-modal");
+  await t.click(addCubeBtn);
+
+  const selectCubeSource = Selector("select");
+
+  //https://testcafe-discuss.devexpress.com/t/how-can-i-select-a-dropdown-option/69/12
+  await t.click(selectCubeSource).click(selectCubeSource.find("option").nth(2));
+
+  const selectCubeEngine = Selector("#db-engine");
+  // const servername = Selector("#servername");
+  // const port = Selector("#port");
+  // const user = Selector("#username");
+  // const pass = Selector("#password");
+  const showDatabasesBtn = Selector("#show-databases-btn");
+
+  await t
+    //by default, those variables are set
+    // .typeText(servername, "localhost")
+    // .typeText(port, "5432")
+    // .typeText(user, "postgres")
+    // .typeText(pass, "root")
+
+    .click(selectCubeEngine)
+    .click(selectCubeEngine.find("option").nth(1))
+    .click(showDatabasesBtn)
+    .expect(Selector("#available-databases").innerText)
+    .contains("postgres");
 });
