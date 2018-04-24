d86cbdde2dd57d1aaf2e58d5efd9c5eedf7147a0
mouadhkaabachi <mouadh.kaabachi@esprit.tn>
2018-04-24 13:06:17 +0100

    improve available databases test

--- front/tests/unit/test_available_dbms.spec.js
+++ front/tests/unit/test_available_dbms.spec.js
@@ -1,9 +1,9 @@
 import { mount } from "vue-test-utils";
-import ConnectDB from "@/cubes/base-schema-options";
+import schemaOptions from "@/cubes/base-schema-options";
+import ConnectDB from "@/cubes/add_cube/connect-db";
 
-describe("ConnectDB", () => {
-  const wrapper = mount(ConnectDB);
-
+describe("schemaOptions", () => {
+  const wrapper = mount(schemaOptions);
   const vm = wrapper.vm;
 
   it("check base-schema-options data", () => {
@@ -11,5 +11,24 @@ describe("ConnectDB", () => {
     expect(vm.showModal).toBe(false);
     expect(vm.cube).toBe("");
     expect(vm.dbConfig).toBe("");
+  });
+});
+
+describe("ConnectDB", () => {
+  test("available databases", () => {
+    const wrapper = mount(ConnectDB);
+    let available_databases = Array.apply(
+      null,
+      wrapper.find("select").element.options
+    ).map(function(el) {
+      return el.text;
+    });
+    expect(available_databases).toEqual([
+      "Choisissez",
+      "Postgres",
+      "Mysql",
+      "Oracle",
+      "SQL Server",
+    ]);
   });
 });
