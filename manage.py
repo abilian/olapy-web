0e13a7fa59b6dba0c171ff2bd2b30dd0a449c339
mouadhkaabachi <mouadh.kaabachi@esprit.tn>
2018-04-25 17:28:27 +0100

    fix UnicodeDecodeError for query builder with labster data

--- manage.py
+++ manage.py
@@ -1,6 +1,8 @@
 from __future__ import absolute_import, division, print_function
 
+import imp
 import os
+import sys
 
 import click
 from app.app import create_app, db
@@ -46,4 +48,10 @@ def run(host, port):
 
 
 if __name__ == '__main__':
+    try:
+        imp.reload(sys)
+        sys.setdefaultencoding("UTF8")
+    except Exception:
+        pass
+
     app.run(host='127.0.0.1', port=5000)
