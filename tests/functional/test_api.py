183e0106976ae33e503f07bf6eb7098527eb377a
mouadhkaabachi <mouadh.kaabachi@esprit.tn>
2018-04-27 18:44:46 +0100

    fix tests

--- tests/functional/test_api.py
+++ tests/functional/test_api.py
@@ -3,8 +3,8 @@ import os
 from os import listdir
 
 from os.path import isfile, join
+from src.olapy_web.api.views import get_cube_source_type, get_config
 
-from olapy_web.api.views import get_cube_source_type, get_config
 from tests.utils import chart_data
 
 CUBE_PATH = 'tests/demo_csv_cubes/sales'
