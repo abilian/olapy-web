fb29fbf249093f3515da5045f58210fef05eaff7
Mouadh Kaabachi <mouadh.kaabachi@esprit.tn>
2019-02-05 17:33:45 +0100

    format

--- olapy_web/extensions.py
+++ olapy_web/extensions.py
@@ -1,5 +1,4 @@
-from __future__ import absolute_import, division, print_function, \
-    unicode_literals
+from __future__ import absolute_import, division, print_function, unicode_literals
 
 from flask_login import LoginManager
 from flask_migrate import Migrate
--- olapy_web/forms.py
+++ olapy_web/forms.py
@@ -1,5 +1,4 @@
-from __future__ import absolute_import, division, print_function, \
-    unicode_literals
+from __future__ import absolute_import, division, print_function, unicode_literals
 
 from flask_wtf import FlaskForm
 from wtforms import BooleanField, PasswordField, StringField, SubmitField
